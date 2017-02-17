16ded20806c496aeaddd7be2fce1d6b4fb5e41c0
ildar shafigullin <ildar66@inbox.ru>
2017-02-17 16:19:34 +0300

    Navigator.

---
+++ Generics/src/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Part I. Generics
+ * *
+ * 1. Introduction:
+ * 1.1 Generics {@link chapter01.section01}
+ * 1.2 Boxing and Unboxing {@link chapter01.section02}
+ * 1.3 Foreach {@link chapter01.section03}
+ * 1.4 Generic Methods and Varargs {@link chapter01.section04}
+ * 1.5 Assertions {@link chapter01.section05}
+ * *
+ * 2. Subtyping and Wildcards:
+ * 2.1 Subtyping and the Substitution Principle {@link chapter02.section01}
+ * 2.2 Wildcards with extends {@link chapter02.section02}
+ * 2.3 Wildcards with super {@link chapter02.section03}
+ * 2.4 The Get and Put Principle {@link chapter02.section04}
+ * 2.5 Arrays {@link chapter02.section05}
+ * 2.6 Wildcards Versus Type Parameters {@link chapter02.section06}
+ * 2.7 Wildcard Capture {@link chapter02.section07}
+ * 2.8 Restrictions on Wildcards {@link chapter02.section08}.
+ * *
+ */
