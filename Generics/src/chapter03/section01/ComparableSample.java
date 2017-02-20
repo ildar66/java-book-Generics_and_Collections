00df49c9ddf9b25c1419062a13edeff7fd27fc26
ildar shafigullin <ildar66@inbox.ru>
2017-02-20 13:25:34 +0300

    Comparable. Summary.

--- Generics/src/chapter03/section01/ComparableSample.java
+++ Generics/src/chapter03/section01/ComparableSample.java
@@ -1,5 +1,7 @@
 package chapter03.section01;
 
+import java.math.BigDecimal;
+
 /**
  * Comparable examples.
  */
@@ -29,5 +31,31 @@ public class ComparableSample {
         Number n = new Double(3.14);
         // assert m.compareTo(n) < 0; // compile-time error (Number class does not implement the Comparable interface.)
 
+        // for java.math.BigDecimal, where the natural ordering is not consistent with equals:
+        BigDecimal a = new BigDecimal("4.00");
+        BigDecimal b = new BigDecimal("4.0");
+        System.out.println("a.equals(b) = " + a.equals(b)); // a.equals(b) = false
+        System.out.println("a.compareTo(b) = " + a.compareTo(b)); // a.compareTo(b) = 0
+
     }
+
+    /*class Integer implements Comparable<Integer> {
+        ...
+        public int compareTo(Integer that) {
+            return this.value < that.value ? -1 :
+                   this.value == that.value ? 0 : 1 ;
+        }
+        ...
+    }*/
+    
+    // But this code may give the wrong answer when there is overflow:
+    /*
+    class Integer implements Comparable<Integer> {
+        ...
+        public int compareTo(Integer that) {
+            // bad implementation -- don't do it this way!
+            return this.value - that.value;
+        }
+        ...
+    }*/
 }
