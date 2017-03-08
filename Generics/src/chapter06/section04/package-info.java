695b9624e966649d1692de35de475b21ba2c8603
ildar shafigullin <ildar66@inbox.ru>
2017-03-08 14:21:50 +0300

    Array Creation. Summary.

--- Generics/src/chapter06/section04/package-info.java
+++ Generics/src/chapter06/section04/package-info.java
@@ -8,5 +8,13 @@
  * Because arrays must reify their component types,
  * it is an error to create a new array unless its component type is reifiable.
  * @see chapter06.section04.Annoying
+ * *
+ * Inability to create generic arrays is one of the most serious restrictions in Java.
+ * Because it is so annoying, it is worth reiterating the reason it occurs:
+ * generic arrays are problematic because generics are implemented via erasure,
+ * but erasure is beneficial because it eases evolution.
+ * *
+ * By far, the best solution to the problems offered by arrays is to “just say no”:
+ * use collections in preference to arrays.
  */
 package chapter06.section04;
