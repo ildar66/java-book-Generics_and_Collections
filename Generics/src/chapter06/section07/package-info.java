d869d0a53debd349624541bf7051ca2902d7ab73
ildar shafigullin <ildar66@inbox.ru>
2017-03-10 17:01:14 +0300

    How to Define ArrayList.

---
+++ Generics/src/chapter06/section07/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * How to Define ArrayList.
+ * *
+ * We have argued elsewhere that it is usually preferable to use a list than to use an array.
+ * In rare circumstances, you will need to use an array for reasons of efficiency or compatibility.
+ * *
+ * @see chapter06.section07.ArrayListTest
+ * ArrayList here is close to the actual definition in the source released by Sun.
+ * Second way: declare the private array to have type Object[] and use casts to type (E) when retrieving elements from the array.
+ */
+package chapter06.section07;
