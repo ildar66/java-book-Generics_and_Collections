1ceb21a334c3fde2453943fd96ab83139d094181
ildar shafigullin <ildar66@inbox.ru>
2017-03-15 14:53:17 +0300

    Reflection.

---
+++ Generics/src/chapter07/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Reflection.
+ * *
+ * Reflection is the term for a set of features that allows a program to examine its own definition.
+ * *
+ * Reflection has two important ways:
+ * 1) "Generics for reflection" we mean that some of the types used for reflection are now generic types (Class becomes the generic class Class<T>);
+ * 2) "Reflection for generics" we mean that reflection now returns information about generic types.
+ */
+package chapter07;
