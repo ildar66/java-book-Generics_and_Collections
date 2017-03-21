08c159da1dce6214e020b9db96ff0c82a696fe9f
ildar shafigullin <ildar66@inbox.ru>
2017-03-21 15:00:04 +0300

    Effective Generics.

---
+++ Generics/src/chapter08/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Effective Generics.
+ * *
+ * This chapter contains advice on how to use generics effectively in practical coding.
+ * We consider checked collections, security issues, specialized classes, and binary compatibility.
+ */
+package chapter08;
