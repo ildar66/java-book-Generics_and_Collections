bcfa29e6bb014d6823bd0546801f3137e95a8826
ildar shafigullin <ildar66@inbox.ru>
2017-03-26 09:49:37 +0300

    Design Patterns.

---
+++ Generics/src/chapter09/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Design Patterns.
+ * *
+ * This chapter reviews five well-known design patterns —
+ * Visitor,
+ * Interpreter,
+ * Function,
+ * Strategy, and
+ * Subject-Observer
+ * — and shows how they can take advantage of generics.
+ * The Function pattern generalizes the idea behind the Comparator interface.
+ */
+package chapter09;
