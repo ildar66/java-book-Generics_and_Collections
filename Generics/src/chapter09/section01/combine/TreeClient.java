32d91c97ca2eabcde6890372b6b115fb8e269d5b
ildar shafigullin <ildar66@inbox.ru>
2017-03-27 16:14:20 +0300

    Design Patterns.Visitor.A tree in practice(with combine visitors and simple approach).

--- Generics/src/chapter09/section01/combine/TreeClient.java
+++ Generics/src/chapter09/section01/combine/TreeClient.java
@@ -18,9 +18,24 @@ class TreeClient {
         });
     }
 
+    // or
+
+    final static Tree.Visitor<Integer, Double> SUM_VISITOR = new Tree.Visitor<Integer, Double>() {
+
+        public Double leaf(Integer e) {
+            return e.doubleValue();
+        }
+
+        public Double branch(Double l, Double r) {
+            return l + r;
+        }
+    };
+
     public static void main(String[] args) {
         Tree<Integer> t = Tree.branch(Tree.branch(Tree.leaf(1), Tree.leaf(2)), Tree.leaf(3));
         System.out.println("combine visitors and simple: " + t + "; sum() = " + sum(t));
+        // or
+        System.out.println("combine visitors and simple: " + t + "; sum() = " + t.visit(SUM_VISITOR));
         assert t.equals("((1^2)^3)");
         assert sum(t) == 6;
     }
