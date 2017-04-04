bb63153e8895b557cf4af3778aa4855de99377c6
ildar shafigullin <ildar66@inbox.ru>
2017-04-04 18:13:27 +0300

    PART II. Collections.The Main Interfaces of the Java Collections Framework.

--- Collections/src/package-info.java
+++ Collections/src/package-info.java
@@ -1,7 +1,8 @@
 /**
  * PART II. Collections.
  * *
- * Figure 10-1 shows the main interfaces of the Java Collections Framework,
- * together with one other—Iterable—which is outside the Framework but is an essential adjunct to it. Its purpose is as follows:
+ * Main interfaces of the Java Collections Framework,
+ * together with one other — Iterable — which is outside the Framework but is an essential adjunct to it.
+ * Its purpose is as follows:
  * • Iterable defines the contract that a class has to fulfill for its instances to be usable with the foreach statement.
  */
