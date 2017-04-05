05f3f50d611cfc18fd562592c96e37ca290fa77a
ildar shafigullin <ildar66@inbox.ru>
2017-04-05 17:03:24 +0300

    Preliminaries.

---
+++ Collections/src/chapter11/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Preliminaries.
+ * *
+ * In this chapter, we will take time to discuss the concepts underlying the framework,
+ * before we get into the detail of the collections themselves.
+ */
+package chapter11;
