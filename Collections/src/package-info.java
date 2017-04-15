67645d69cba369f9c9fbd85fd6072352c404931e
ildar shafigullin <ildar66@inbox.ru>
2017-04-15 15:27:46 +0300

    PART II. Collections.

--- Collections/src/package-info.java
+++ Collections/src/package-info.java
@@ -1,4 +1,16 @@
 /**
  * PART II. Collections.
  * *
+ * 10. The Main Interfaces of the Java Collections Framework {@link chapter10}
+ * *
+ * 11. Preliminaries:
+ * 11.1 Iterable and Iterators {@link chapter11.section01}
+ * 11.2 Implementations {@link chapter11.section02}
+ * 11.3 Efficiency and the O-Notation {@link chapter11.section03}
+ * 11.4 Contracts {@link chapter11.section04}
+ * 11.5 Collections and Thread Safety {@link chapter11.section05}
+ * 11.5.1 Synchronization and the Legacy Collections {@link chapter11.section05.part_1}
+ * 11.5.2 JDK 1.2: Synchronized Collections and Fail-Fast Iterators {@link chapter11.section05.part_2}
+ * 11.5.3 Concurrent Collections: Java 5 and Beyond {@link chapter11.section05.part_3}
+ * *
  */
