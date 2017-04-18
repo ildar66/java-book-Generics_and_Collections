e9835179764a48bac5c4751fed3581ec8ed0add2
ildar shafigullin <ildar66@inbox.ru>
2017-04-18 17:28:27 +0300

    Using the Methods of Collection. Example tasks and task collections for the task manager.

--- Collections/src/chapter12/section01/package-info.java
+++ Collections/src/chapter12/section01/package-info.java
@@ -14,6 +14,12 @@
  * In accordance with good practice, we have made both kinds of task immutable by declaring the fields to be final,
  * and we have declared both subclasses to be final so that no one can later define mutable subclasses
  * (see item “Minimize mutability”/“Favor immutability”) in Chapter 4 of Effective Java by Joshua Bloch, Addison-Wesley).
+ * *
  * We also define an empty task: {@link chapter12.section01.EmptyTask}
+ * Since the empty string precedes all others in the natural ordering on strings,
+ * the empty task comes before all others in the natural ordering on tasks.
+ * This task will be useful when we construct range views of sorted sets (see chapter13.2).
+ * *
+ * Example tasks and task collections for the task manager: {@link chapter12.section01.StaticTestData}
  */
 package chapter12.section01;
