77cc97b19b9162175bf79a72e5ff5e508cb178db
ildar shafigullin <ildar66@inbox.ru>
2017-04-18 17:06:19 +0300

    Using the Methods of Collection. Task.

---
+++ Collections/src/chapter12/section01/Task.java
@@ -0,0 +1,23 @@
+package chapter12.section01;
+
+public abstract class Task implements Comparable<Task> {
+
+    protected Task() {
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Task) {
+            return toString().equals(o.toString());
+        } else return false;
+    }
+
+    public int compareTo(Task t) {
+        return toString().compareTo(t.toString());
+    }
+
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    public abstract String toString();
+}
---
+++ Collections/src/chapter12/section01/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Using the Methods of Collection.
+ * *
+ * To illustrate the use of the collection classes, let’s construct a tiny example.
+ * @see chapter12.section01.Task
+ * We only require four operations on tasks: equals, compareTo, hashCode and toString.
+ * Equality will be used to test whether a collection contains a given task,
+ * comparison will be used by ordered collections (such as OrderedSet and OrderedMap)
+ * and the hash code will be used by collections based on hash tables (such as HashSet and HashMap),
+ * and the string representation of a task will be used whenever we show the contents of a collection.
+ */
+package chapter12.section01;
