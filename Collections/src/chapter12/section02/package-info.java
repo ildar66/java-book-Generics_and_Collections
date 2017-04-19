f746b048f5b6f0d8aee205c3e6c458460bd11055
ildar shafigullin <ildar66@inbox.ru>
2017-04-19 16:17:46 +0300

    Implementing Collection.

---
+++ Collections/src/chapter12/section02/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Implementing Collection.
+ * *
+ * There are no concrete implementations of {@link java.util.Collection}.
+ * The class {@link java.util.AbstractCollection}, which partially implements it, is one of a series of skeletal implementations —
+ * including {@link java.util.AbstractSet}, {@link java.util.AbstractList}, and so on —
+ * which provide functionality common to the different concrete implementations of each interface.
+ * These skeletal implementations are available to help the designer of new implementations of the Framework interfaces.
+ * *
+ * For example, {@link java.util.Collection} could serve as the interface for bags (unordered lists),
+ * and a programmer implementing bags could extend {@link java.util.AbstractCollection} and find most of the implementation work already done.
+ */
+package chapter12.section02;
