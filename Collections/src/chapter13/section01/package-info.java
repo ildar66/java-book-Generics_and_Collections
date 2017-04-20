66ccfef252ed69497b8d0ac1a566c09b1f0cb4d5
ildar shafigullin <ildar66@inbox.ru>
2017-04-20 15:33:55 +0300

    Implementing Set.

---
+++ Collections/src/chapter13/section01/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Implementing Set.
+ * *
+ * There are six concrete implementations of {@link java.util.Set} in the Collections Framework.
+ * {@link java.util.Set} and its sub-interfaces {@link java.util.SortedSet} and {@link java.util.NavigableSet}.
+ * *
+ * In this section, we will look at
+ * {@link java.util.HashSet},
+ * {@link java.util.LinkedHashSet},
+ * {@link java.util.concurrent.CopyOnWriteArraySet} and
+ * {@link java.util.EnumSet}.
+ * *
+ * We will discuss {@link java.util.SortedSet} and {@link java.util.NavigableSet}, together with their implementations,
+ * {@link java.util.TreeSet} and {@link java.util.concurrent.ConcurrentSkipListSet}, in section02.
+ */
+package chapter13.section01;
