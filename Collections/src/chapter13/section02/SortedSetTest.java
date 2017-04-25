904287a31566333325658e4d891c37e1782edd75
ildar shafigullin <ildar66@inbox.ru>
2017-04-25 17:57:08 +0300

    SortedSet and NavigableSet. SortedSetTest.

--- Collections/src/chapter13/section02/SortedSetTest.java
+++ Collections/src/chapter13/section02/SortedSetTest.java
@@ -13,44 +13,43 @@ import static chapter12.section01.StaticTestData.*;
 public class SortedSetTest {
 
     public static void main(String[] args) {
+        // 1:
         Set<Task> naturallyOrderedTasks = new TreeSet<Task>(mondayTasks);
         naturallyOrderedTasks.addAll(tuesdayTasks);
         assert naturallyOrderedTasks.toString().equals("[code db, code gui, code logic, phone Mike, phone Paul]");
 
+        // 2:
         NavigableSet<PriorityTask> priorityTasks = new TreeSet<PriorityTask>();
         priorityTasks.add(new PriorityTask(mikePhone, Priority.MEDIUM));
         priorityTasks.add(new PriorityTask(paulPhone, Priority.HIGH));
         priorityTasks.add(new PriorityTask(databaseCode, Priority.MEDIUM));
         priorityTasks.add(new PriorityTask(interfaceCode, Priority.LOW));
-        assert (priorityTasks.toString()).equals(
-                "[phone Paul: HIGH, code db: MEDIUM, phone Mike: MEDIUM, code gui: LOW]");
-        PriorityTask firstLowPriorityTask =
-                new PriorityTask(new EmptyTask(), Priority.LOW);
-        SortedSet<PriorityTask> highAndMediumPriorityTasks =
-                priorityTasks.headSet(firstLowPriorityTask);
-        assert highAndMediumPriorityTasks.toString().equals(
-                "[phone Paul: HIGH, code db: MEDIUM, phone Mike: MEDIUM]");
-        PriorityTask firstMediumPriorityTask =
-                new PriorityTask(new EmptyTask(), Priority.MEDIUM);
-        SortedSet<PriorityTask> mediumPriorityTasks =
-                priorityTasks.subSet(
-                        firstMediumPriorityTask, firstLowPriorityTask);
-        assert mediumPriorityTasks.toString().equals(
-                "[code db: MEDIUM, phone Mike: MEDIUM]");
-        PriorityTask logicCodeMedium =
-                new PriorityTask(logicCode, Priority.MEDIUM);
+        assert (priorityTasks.toString()).equals("[phone Paul: HIGH, code db: MEDIUM, phone Mike: MEDIUM, code gui: LOW]");
+
+        // 3:
+        PriorityTask firstLowPriorityTask = new PriorityTask(new EmptyTask(), Priority.LOW);
+        SortedSet<PriorityTask> highAndMediumPriorityTasks = priorityTasks.headSet(firstLowPriorityTask);
+        assert highAndMediumPriorityTasks.toString().equals("[phone Paul: HIGH, code db: MEDIUM, phone Mike: MEDIUM]");
+
+        // 4:
+        PriorityTask firstMediumPriorityTask = new PriorityTask(new EmptyTask(), Priority.MEDIUM);
+        SortedSet<PriorityTask> mediumPriorityTasks = priorityTasks.subSet(firstMediumPriorityTask, firstLowPriorityTask);
+        assert mediumPriorityTasks.toString().equals("[code db: MEDIUM, phone Mike: MEDIUM]");
+
+        // 5:
+        PriorityTask logicCodeMedium = new PriorityTask(logicCode, Priority.MEDIUM);
         priorityTasks.add(logicCodeMedium);
-        assert mediumPriorityTasks.toString().equals(
-                "[code db: MEDIUM, code logic: MEDIUM, phone Mike: MEDIUM]");
+        assert mediumPriorityTasks.toString().equals("[code db: MEDIUM, code logic: MEDIUM, phone Mike: MEDIUM]");
+
+        // 6:
         mediumPriorityTasks.remove(logicCodeMedium);
-        assert priorityTasks.toString().equals(
-                "[phone Paul: HIGH, code db: MEDIUM, phone Mike: MEDIUM, code gui: LOW]");
+        assert priorityTasks.toString().equals("[phone Paul: HIGH, code db: MEDIUM, phone Mike: MEDIUM, code gui: LOW]");
+
+        // 7:
         PriorityTask nextTask = priorityTasks.pollFirst();
         assert nextTask.toString().equals("phone Paul: HIGH");
         PriorityTask mikePhoneMedium = new PriorityTask(mikePhone, Priority.MEDIUM);
-        NavigableSet closedInterval = priorityTasks.subSet(
-                firstMediumPriorityTask, true, mikePhoneMedium, true);
-        assert (closedInterval.toString()).equals(
-                "[code db: MEDIUM, phone Mike: MEDIUM]");
+        NavigableSet closedInterval = priorityTasks.subSet(firstMediumPriorityTask, true, mikePhoneMedium, true);
+        assert (closedInterval.toString()).equals("[code db: MEDIUM, phone Mike: MEDIUM]");
     }
 }
--- Collections/src/chapter13/section02/package-info.java
+++ Collections/src/chapter13/section02/package-info.java
@@ -23,7 +23,7 @@
  * *
  * Now merging two ordered lists, which was quite tricky using parallel iterators, is trivial if we get a {@link java.util.SortedSet} to do the work.
  * Using the task collections of Example {@link chapter12.section01.StaticTestData}, it requires two lines of code:
- * @see chapter13.section02.SortedSetTest#main(java.lang.String[])
+ * @see chapter13.section02.SortedSetTest#main(java.lang.String[])(// 1:)
  * This simplicity comes at a price, though;
  * merging two sorted lists of size n is O(n), but adding n elements to a {@link java.util.TreeSet} of size n is O(n log n).
  * *
@@ -35,5 +35,66 @@
  * To test whether two PriorityTasks are equal, we check whether they have the same priority and the same task.
  * These definitions ensure that the natural ordering is consistent with equals (see chapter03.1).
  * As when we defined tasks in chapter12.1, we have followed good practice by making {@link chapter13.section02.PriorityTask} immutable.
+ * *
+ * The following code shows {@link java.util.SortedSet} working with a set of PriorityTasks.
+ * (in fact, we have declared a {@link java.util.NavigableSet} so that we can use the same set in later examples.
+ * But for the moment, we will just use the methods of {@link java.util.SortedSet}):
+ * @see chapter13.section02.SortedSetTest#main(java.lang.String[])(// 2:)
+ * *
+ * Could you not simply compare the priorities of the tasks, without using the string representation as a secondary key?
+ * A partial ordering like that would be useful if you want to preserve some aspects of the original ordering;
+ * for example, you might wish to sort tasks by priority but, within each priority, preserve the order in which they were added to the set.
+ * But the contract for SortedSet (and, as we shall see later, SortedMap) states that it will use the compare method of its Comparator — or,
+ * if it does not have one, the compareTo method of its elements — instead of the elements’ equals method to determine when elements are distinct.
+ * This means that if a number of elements compare as the same, the set will treat them as duplicates, and all but one will be discarded.
+ * *
+ * The methods defined by the {@link java.util.SortedSet} interface fall into three groups:
+ * ********** Getting the First and Last Elements *********
+ * E first() // return the first element in the set
+ * E last() // return the last element in the set
+ * If the set is empty, these operations throw NoSuchElementException.
+ * ********** Retrieving the Comparator ***********
+ * Comparator<? super E> comparator()
+ * This method returns the set’s comparator if it has been given one at construction time.
+ * The type Comparator<? super E> is used because a SortedSet parameterized on E can rely for ordering on a Comparator defined on any supertype of E.
+ * For example, recalling chapter3.3, a Comparator<Fruit> could be used with a SortedSet<Apple>.
+ * ********** Getting Range Views ****************
+ * SortedSet<E> subSet(E fromElement, E toElement)
+ * SortedSet<E> headSet(E toElement)
+ * SortedSet<E> tailSet(E fromElement)
+ * The method subSet returns a set containing every element of the original set that is greater than or equal to fromElement and less than toElement.
+ * Similarly, the method "headSet" returns every element that is less than toElement,
+ * and "tailSet" returns every element that is greater than or equal to fromElement.
+ * Note that the arguments to these operations do not themselves have to be members of the set.
+ * The sets returned are halfopen intervals:
+ * they are inclusive of the fromElement — provided it actually is a set member, of course — and exclusive of the toElement.
+ * *
+ * In our example, these methods could be useful in providing different views of the elements in priorityTasks.
+ * For instance, we can use headSet to obtain a view of the highand medium-priority tasks.
+ * To do this, we need a special task that comes before all others in the task ordering;
+ * fortunately, we defined a class {@link chapter12.section01.EmptyTask} for just this purpose in chapter12.1.
+ * Using this, it is easy to extract all tasks that come before any lowpriority task:
+ * @see chapter13.section02.SortedSetTest#main(java.lang.String[])(// 3:)
+ * *
+ * In fact,
+ * because we know that tasks with empty details will never normally occur, we can also use one as the first endpoint in a half-open interval:
+ * @see chapter13.section02.SortedSetTest#main(java.lang.String[])(// 4:)
+ * *
+ * Not all orderings can be treated so conveniently;
+ * suppose, for example, that we want to work with the set of all the medium-priority tasks up to and including the mike Phone task.
+ * To define that set as a half-open interval,
+ * users of SortedSet would need to construct the task that immediately follows the mikePhone task in the PriorityTask ordering, and for that
+ * you would need to know that the string that succeeds "Mike" in the natural ordering is "Mike\0" (that is, "Mike" with a null character appended).
+ * Fortunately, users of {@link java.util.NavigableSet} have a much more intuitive way of defining this set, as we shall see in a moment.
+ * Notice that the sets returned by these operations are not independent sets but new views of the original {@link java.util.SortedSet}.
+ * So we can add elements to the original set and see the changes reflected in the view:
+ * @see chapter13.section02.SortedSetTest#main(java.lang.String[])(// 5:)
+ * *
+ * The reverse applies also; changes in the view are reflected in the original set:
+ * @see chapter13.section02.SortedSetTest#main(java.lang.String[])(// 6:)
+ * *
+ * To understand how this works, think of all the possible values in an ordering as lying on a line, like the number line used in arithmetic.
+ * A range is defined as a fixed segment of that line, regardless of which values are actually in the original set.
+ * So a subset, defined on a SortedSet and a range, will allow you to work with whichever elements of the SortedSet currently lie within the range.
  */
 package chapter13.section02;
