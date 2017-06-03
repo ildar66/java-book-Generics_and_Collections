d80aed8c18fbf0457426c125ba2a19f2c992ba88
ildar shafigullin <ildar66@inbox.ru>
2017-06-03 09:45:05 +0300

    "Queues" table content.

--- Collections/src/package-info.java
+++ Collections/src/package-info.java
@@ -29,4 +29,17 @@
  * 13.2.2 TreeSet {@link chapter13.section02.part_2}
  * 13.2.3 ConcurrentSkipListSet {@link chapter13.section02.part_3}
  * 13.3 Comparing Set Implementations {@link chapter13.section03}
+ * *
+ * 14. Queues:
+ * 14.1 Using the Methods of Queue {@link chapter14.section01}
+ * 14.2 Implementing Queue {@link chapter14.section02}
+ * 14.2.1 PriorityQueue {@link chapter14.section02.part_1}
+ * 14.2.2 ConcurrentLinkedQueue {@link chapter14.section02.part_2}
+ * 14.3 BlockingQueue {@link chapter14.section03}
+ * 14.3.1 Using the Methods of BlockingQueue {@link chapter14.section03.part_1}
+ * 14.3.2 Implementing BlockingQueue {@link chapter14.section03.part_2}
+ * 14.4 Deque {@link chapter14.section04}
+ * 14.4.1 Implementing Deque {@link chapter14.section04.part_1}
+ * 14.4.2 BlockingDeque {@link chapter14.section04.part_2}
+ * 14.5 Comparing Queue Implementations {@link chapter14.section05}
  */
