cd5083f0dd9562363627715df9d776ad2e545d80
ildar shafigullin <ildar66@inbox.ru>
2017-06-10 07:54:33 +0300

    Implementing List.

---
+++ Collections/src/chapter15/section02/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Implementing {@link java.util.List}.
+ * *
+ * There are three concrete implementations of List in the Collections Framework,
+ * differing in how fast they perform the various operations defined by the interface and how they behave in the face of concurrent modification;
+ * unlike {@link java.util.Set} and {@link java.util.Queue}, however, {@link java.util.List} has no subinterfaces to specify differences in
+ * functional behavior.
+ * *
+ * In this and the following section we look at each implementation in turn and provide a performance comparison.
+ */
+package chapter15.section02;
