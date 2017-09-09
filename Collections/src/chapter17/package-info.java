36d21bcc6edefe62ebb20cf0d15d518b73988653
ildar shafigullin <ildar66@inbox.ru>
2017-09-09 09:14:40 +0300

    The Collections Class.

---
+++ Collections/src/chapter17/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The Collections Class.
+ * *
+ * The class java.util.Collections consists entirely of static methods that operate on or return collections.
+ * There are three main categories:
+ * * generic algorithms,
+ * * methods that return empty or pre-populated collections,
+ * * and methods that create wrappers.
+ * We discuss these three categories in turn, followed by a number of other methods which do not fit into a neat classification.
+ * All the methods of Collections are public and static, so for readability we will omit these modifiers from the individual declarations.
+ */
+package chapter17;
