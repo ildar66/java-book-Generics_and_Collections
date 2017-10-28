75935ae3ce41aa94c13b2dc5cc09a721647387e2
ildar shafigullin <ildar66@inbox.ru>
2017-10-28 12:24:30 +0300

    Generic Algorithms.

---
+++ Collections/src/chapter17/section01/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Generic Algorithms.
+ * *
+ * The generic algorithms fall into four major categories:
+ * changing element order in a list,
+ * changing the contents of a list,
+ * finding extreme values in a collection,
+ * and finding specific values in a list.
+ * They represent reusable functionality, in that they can be applied to Lists (or in some cases to Collections) of any type.
+ * Generifying the types of these methods has led to some fairly complicated declarations,
+ * so each section discusses the declarations briefly after presenting them.
+ */
+package chapter17.section01;
